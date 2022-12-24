71d934c8bef9d38f9d4c3a5181f3f33f8837d80a
Rob Rainthorpe <robr@robrainthorpe.com>
2022-12-23 18:14:46 -0800

    more tests

--- test_puzzle.py
+++ test_puzzle.py
@@ -1,17 +1,20 @@
 import pytest
 from puzzle import Puzzle
+import argparse
 
 
 class TestPuzzle:
     good_letters = "ABCDEFGHI"
-    test_words= 'test_word_list.txt'
+    test_words = "test_word_list.txt"
 
     def test_grid_must_be_square(self):
-        with pytest.raises(Exception):
+        with pytest.raises(
+            Exception, match="Length of letters must be a square number.*"
+        ):
             puzzle = Puzzle("ABC", word_list=self.test_words)
 
     def test_puzzle_created(self):
-        puzzle = Puzzle(self.good_letters)
+        puzzle = Puzzle(self.good_letters, word_list=self.test_words)
         pass
 
     def test_grid_loaded_correctly(self):
@@ -23,4 +26,42 @@ class TestPuzzle:
         puzzle2 = Puzzle("ABCDEFGHIJKLMNOP", word_list=self.test_words)
 
         assert puzzle.word_list_length() == 1345
-        assert puzzle2.word_list_length() == 2519
+        assert puzzle2.word_list_length() == 2517
+
+    def test_solution_includes_all_letter_word(self):
+        puzzle = Puzzle("HTEZRONIOPAHMORP", word_list=self.test_words)
+        puzzle.solve()
+
+        assert "ANTHROPOMORPHIZE" in puzzle.raw_solutions()
+
+    def test_solution_excludes_unlinked_words(self):
+        puzzle = Puzzle("HTEZRONIOPAHMORP", word_list=self.test_words)
+        puzzle.solve()
+
+        assert not "OPERA" in puzzle.raw_solutions()
+
+    def test_exception_if_solve_called_before_getting_solutions(self):
+        puzzle = Puzzle("ABCD", word_list=self.test_words)
+
+        with pytest.raises(Exception, match=".solve\(\) must be called before.*"):
+            puzzle.raw_solutions()
+
+        with pytest.raises(Exception, match=".solve\(\) must be called before.*"):
+            puzzle.print_solutions({'sort': False})
+
+    def test_can_get_or_print_solutions_if_solve_called(self):
+        puzzle = Puzzle(self.good_letters, word_list=self.test_words)
+        puzzle.solve()
+
+        solutions = puzzle.raw_solutions()
+
+        assert len(solutions) > 0
+
+        puzzle.print_solutions(
+            {
+                "single_column": True,
+                "length": True,
+                "headers": False,
+                "sort": True,
+            }
+        )
